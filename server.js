e54cdac3aecb73dd711de037c0be4dd87b18da6a
Kurt <kurtvarcoe@gmail.com>
2022-02-24 20:58:24 +0200

    added try catch block 404

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
+const { unescape } = require('querystring');
 
 const mimeTypes = {
     "html": "text/html",
@@ -12,15 +13,16 @@ const mimeTypes = {
     "png": "image/png"
 };
 
-const hostname = '127.0.0.1';
-const port = 1337;
+// const hostname = '127.0.0.1';
+// const port = 1337;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// })
